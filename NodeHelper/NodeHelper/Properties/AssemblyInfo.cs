400a99fdab900333c0eded89a0b8e2c7a5b63984
Phineas Freak <precise.timing.labs@gmail.com>
2018-03-09 13:35:23 +0200

    Recompile for KSP 1.4

--- NodeHelper/NodeHelper/Properties/AssemblyInfo.cs
+++ NodeHelper/NodeHelper/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion ("1.3.1.1891")]
-[assembly: AssemblyFileVersion ("1.3.1.2")]
+[assembly: AssemblyVersion ("1.4.0.2077")]
+[assembly: AssemblyFileVersion ("1.4.0.1")]
