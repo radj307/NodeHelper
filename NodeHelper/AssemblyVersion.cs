56d62b6d69c09ab56763f960e0e4f6be353464bd
radj307 <radj307@gmail.com>
2022-01-11 17:41:03 -0500

    Update to .NET 4.8

--- NodeHelper/AssemblyVersion.cs
+++ NodeHelper/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.5.1.4")]
- [assembly: AssemblyFileVersion("1.5.1.4")]
+ [assembly: AssemblyVersion("1.5.1.5")]
+ [assembly: AssemblyFileVersion("1.5.1.5")]
